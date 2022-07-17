3b1f672634d3f1d68133d4f08047dfd8d3001d52
demola malomo <malomo.alaba@yahoo.com>
2022-07-17 13:32:34 +0100

    ui cleanup 2

--- components/Item.js
+++ components/Item.js
@@ -3,11 +3,7 @@ import React from 'react';
 export const Item = ({ img, title, price }) => {
   return (
     <div className='h-full border border-gray-600 rounded-lg p-6'>
-      <img
-        alt={img}
-        src={img}
-        className='h-64 w-full object-fit mb-4'
-      />
+      <img alt={title} src={img} className='h-64 w-full object-fit mb-4' />
       <p className='text-gray-800 mb-1 text-sm'>{title}</p>
       <h5 className='text-gray-800 mb-1 font-medium text-lg'>$ {price}</h5>
     </div>
